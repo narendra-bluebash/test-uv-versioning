1ba751286f793fb83cbdcc3b18322b40bbe86912
narendra-bluebash <narendra.dangi@bluebash.co>
2025-12-02 11:23:13 +0530

    dkkjff

---
+++ hello.py
@@ -0,0 +1,14 @@
+# def main():
+#     print("Hello from test-uv-versioning!")
+
+from importlib.metadata import version
+
+def main():
+    print(version("test-uv-versioning"))
+
+
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ src/lol.py
@@ -0,0 +1,7 @@
+def todo():
+    print("This is a placeholder function.")
+    
+    
+    
+if __name__ == "__main__":
+    todo()
